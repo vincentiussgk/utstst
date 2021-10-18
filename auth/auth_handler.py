52e35502809c1e4690ece01ca9a26e582bac052b
f1reboywatergrl <vincentius.samuelgk2@gmail.com>
2021-10-18 10:48:43 +0700

    test redeploy

--- auth/auth_handler.py
+++ auth/auth_handler.py
@@ -4,10 +4,8 @@ from typing import Dict
 import jwt
 from decouple import config
 
-
 JWT_SECRET = config("secret")
 JWT_ALGORITHM = config("algorithm")
-
 
 def token_response(token: str):
     return {
